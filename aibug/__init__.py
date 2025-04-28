b6925a6d3abe65d40ec5a73da1207616afea13ef
Tony <tonytasks@gmail.com>
2025-04-28 14:26:46 +0800

    Initialize Python package structure

---
+++ aibug/__init__.py
@@ -0,0 +1,8 @@
+"""
+AIBugReport: Intelligent Bug Tracking CLI
+
+A command-line interface tool for efficient and intelligent bug tracking,
+leveraging LLMs and decentralized storage.
+"""
+
+__version__ = '0.1.0'
